206dbb3e497e8821a28577b51ea56282ff7421e4
Yogesh Palshikar <yogeshpalshikar1992@gmail.com>
2023-06-30 16:58:26 +0530

    added hooks

--- src/App.js
+++ src/App.js
@@ -3,37 +3,35 @@ import './App.css';
 // import { robots } from './robots';
 import CardList from './CardList'
 import Searchbox from './Searchbox'
-import React, {Component} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import Scroll from './Scroll'
 import ErrorBoundry from './ErrorBoundry'
 
-class App extends Component{
-  constructor(){
-    super()
-    this.state = {
-      robots: [],
-      searchfield : ' '
-    }
-  }
 
-  componentDidMount(){
+function App(){
+  const [robots, setRobots] = useState([])
+  const [searchfield, setSearchField] = useState('')
+
+  useEffect(() =>{
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(users => this.setState({robots: users}))
+    .then(users => setRobots(users))    
+  },[])
+
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value)
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value})
-  }
-
-  render(){
-    const filteredRobots = this.state.robots.filter(robots => {
-      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-    })
-    return (
+  const filteredRobots = robots.filter(robots => {
+    return robots.name.toLowerCase().includes(searchfield.toLowerCase())
+  })
+  
+  return !robots.length ?
+    <h1>Loading</h1> :
+    (
       <div className='tc'>
         <h1 className='f1'>Robofriends</h1>
-        <Searchbox searchChange={this.onSearchChange}/>
+        <Searchbox searchChange={onSearchChange}/>
         <Scroll>
           <ErrorBoundry>
             <CardList robots={filteredRobots} />
@@ -42,28 +40,44 @@ class App extends Component{
         
       </div>    
     )
-  }
-}
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
+}
+// class App extends Component{
+//   constructor(){
+//     super()
+//     this.state = {
+//       robots: [],
+//       searchfield : ' '
+//     }
+//   }
+
+//   componentDidMount(){
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(response => response.json())
+//     .then(users => this.setState({robots: users}))
+//   }
+
+//   onSearchChange = (event) => {
+//     this.setState({ searchfield: event.target.value})
+//   }
+
+//   render(){
+//     const filteredRobots = this.state.robots.filter(robots => {
+//       return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+//     })
+//     return (
+//       <div className='tc'>
+//         <h1 className='f1'>Robofriends</h1>
+//         <Searchbox searchChange={this.onSearchChange}/>
+//         <Scroll>
+//           <ErrorBoundry>
+//             <CardList robots={filteredRobots} />
+//           </ErrorBoundry>
+//         </Scroll>
+        
+//       </div>    
+//     )
+//   }
 // }
 
 export default App;
